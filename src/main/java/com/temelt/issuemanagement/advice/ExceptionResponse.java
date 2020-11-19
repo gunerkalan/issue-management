c923d71c17371763ce057d62756a38653137320a
Güner KALAN <gunerkalan@gmail.com>
2020-11-19 16:58:46 +0300

    update 19.11.2020-2

--- src/main/java/com/temelt/issuemanagement/advice/ExceptionResponse.java
+++ src/main/java/com/temelt/issuemanagement/advice/ExceptionResponse.java
@@ -2,9 +2,13 @@ package com.temelt.issuemanagement.advice;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ExceptionResponse {
 	private Date date;
 	private String message;
